72cee7da228a41e6b30db2881a372d2148c6e665
Erik Rasmussen <erik.rasmussen@inetalliance.net>
2015-08-07 10:34:08 +0200

    exported action creators to top level. fixes #12

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import createFormReducer from './createFormReducer';
 import reduxForm from './reduxForm';
+import {change, showAll, reset} from './actions';
 
 export default reduxForm;
 export {
-  createFormReducer
+  createFormReducer,
+  change,
+  showAll,
+  reset
 };
