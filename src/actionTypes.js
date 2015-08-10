13361865c78fa6be9828ab4c6bf53169f7bf52fc
Erik Rasmussen <erik.rasmussen@inetalliance.net>
2015-08-10 17:53:15 +0200

    made action verbs uppercase. #16

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -1,8 +1,8 @@
-export const BLUR = 'redux-form/blur';
-export const CHANGE = 'redux-form/change';
-export const INITIALIZE = 'redux-form/initialize';
-export const RESET = 'redux-form/reset';
-export const TOUCH = 'redux-form/touch';
-export const TOUCH_ALL = 'redux-form/touch-all';
-export const UNTOUCH = 'redux-form/untouch';
-export const UNTOUCH_ALL = 'redux-form/untouch-all';
+export const BLUR = 'redux-form/BLUR';
+export const CHANGE = 'redux-form/CHANGE';
+export const INITIALIZE = 'redux-form/INITIALIZE';
+export const RESET = 'redux-form/RESET';
+export const TOUCH = 'redux-form/TOUCH';
+export const TOUCH_ALL = 'redux-form/TOUCH_ALL';
+export const UNTOUCH = 'redux-form/UNTOUCH';
+export const UNTOUCH_ALL = 'redux-form/UNTOUCH_ALL';
