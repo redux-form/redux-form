16b42cc68cf23a0fed7ab504d091fd9a64d80432
Erik Rasmussen <rasmussenerik@gmail.com>
2015-10-21 02:00:27 +0200

    added placeholders

--- src/examples/SimpleForm.js
+++ src/examples/SimpleForm.js
@@ -8,19 +8,19 @@ const SimpleForm = props => {
       <div className="form-group">
         <label className="col-xs-4 control-label">First Name</label>
         <div className="col-xs-8">
-          <input type="text" className="col-xs-8 form-control" {...firstName}/>
+          <input type="text" className="col-xs-8 form-control" placeholder="First Name" {...firstName}/>
         </div>
       </div>
       <div className="form-group">
         <label className="col-xs-4 control-label">Last Name</label>
         <div className="col-xs-8">
-          <input type="text" className="col-xs-8 form-control" {...lastName}/>
+          <input type="text" className="col-xs-8 form-control" placeholder="Last Name" {...lastName}/>
         </div>
       </div>
       <div className="form-group">
         <label className="col-xs-4 control-label">Email</label>
         <div className="col-xs-8">
-          <input type="text" className="col-xs-8 form-control" {...email}/>
+          <input type="text" className="col-xs-8 form-control" placeholder="Email" {...email}/>
         </div>
       </div>
       <div className="form-group">
@@ -38,6 +38,7 @@ const SimpleForm = props => {
         <label className="col-xs-4 control-label">Favorite Color</label>
         <div className="col-xs-8">
           <select className="form-control" {...favoriteColor}>
+            <option></option>
             <option value="ff0000">Red</option>
             <option value="00ff00">Green</option>
             <option value="0000ff">Blue</option>
