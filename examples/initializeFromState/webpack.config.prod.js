f50a1816a85d44105bb467fd3bafe6ae41f3f28d
Erik Rasmussen <rasmussenerik@gmail.com>
2016-05-10 00:49:20 +0200

    v6.0.0-alpha.8

--- examples/initializeFromState/webpack.config.prod.js
+++ examples/initializeFromState/webpack.config.prod.js
@@ -18,7 +18,7 @@ module.exports = {
       'process.env': {
         NODE_ENV: JSON.stringify('production')
       },
-      REDUX_FORM_VERSION: JSON.stringify('6.0.0-alpha.7')
+      REDUX_FORM_VERSION: JSON.stringify('6.0.0-alpha.8')
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
