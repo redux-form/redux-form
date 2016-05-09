04036bf3385cd6aeefe0bb1b76c850f308919bf4
Florent Cailhol <florent.cailhol@gmail.com>
2016-05-09 17:49:13 +0200

    Improve mapValues and every perfs

--- src/util/every.js
+++ src/util/every.js
@@ -2,12 +2,21 @@
  * Returns true of the predicate is true for every (value, key) pair in the Object or array
  */
 export default function every(object, predicate) {
-  if(!object) {
+  if (object !== Object(object)) {
     return true
   }
-  if(Array.isArray(object)) {
+
+  if (Array.isArray(object)) {
     return !object.length || object.every(predicate)
   }
+
   const keys = Object.keys(object)
-  return !keys.length || keys.every(key => predicate(object[key], key, object))
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    if (!predicate(object[key], key, object)) {
+      return false
+    }
+  }
+
+  return true
 }
--- src/util/mapValues.js
+++ src/util/mapValues.js
@@ -2,10 +2,18 @@
  * Maps all the values in the given object through the given function and saves them, by key, to a result object
  */
 const mapValues = (obj, fn) => {
-  return obj ? Object.keys(obj).reduce((accumulator, key) => ({
-    ...accumulator,
-    [key]: fn(obj[key], key)
-  }), {}) : obj
+  if (obj !== Object(obj)) {
+    return obj
+  } else {
+    const acc = {}
+    const keys = Object.keys(obj)
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i]
+      acc[key] = fn(obj[key], key)
+    }
+
+    return acc
+  }
 }
 
 export default mapValues
