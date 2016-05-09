9f3309c127582bd450c4640e620b4f792f3b40f0
Erik Rasmussen <rasmussenerik@gmail.com>
2016-05-09 18:58:21 +0200

    added test for plain deepequal. related to #929

--- src/structure/plain/index.js
+++ src/structure/plain/index.js
@@ -1,16 +1,15 @@
 import splice from './splice'
 import getIn from './getIn'
 import setIn from './setIn'
+import deepEqual from './deepEqual'
 import deleteIn from './deleteIn'
 import deleteWithPath from './deleteWithPath'
-
-import isEqual from 'lodash/isEqual'
 
 const structure = {
   empty: {},
   getIn,
   setIn,
-  deepEqual: isEqual,
+  deepEqual,
   deleteIn,
   deleteWithPath,
   fromJS: value => value,
