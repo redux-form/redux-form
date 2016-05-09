1c1158617d63706b80fe0b6143a0a09107efded2
Erik Rasmussen <rasmussenerik@gmail.com>
2016-05-09 18:47:13 +0200

    replaced plain deepequal with lodash implementation. related to #929

--- src/structure/plain/index.js
+++ src/structure/plain/index.js
@@ -1,15 +1,16 @@
 import splice from './splice'
 import getIn from './getIn'
 import setIn from './setIn'
-import deepEqual from './deepEqual'
 import deleteIn from './deleteIn'
 import deleteWithPath from './deleteWithPath'
+
+import isEqual from 'lodash/isEqual'
 
 const structure = {
   empty: {},
   getIn,
   setIn,
-  deepEqual,
+  deepEqual: isEqual,
   deleteIn,
   deleteWithPath,
   fromJS: value => value,
