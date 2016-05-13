63f721d97fc2801d03b9f726a4c1efd452c0845f
Erik Rasmussen <rasmussenerik@gmail.com>
2016-05-13 14:25:09 +0200

    made inputs of type select-multiple default to []. fixes #957

--- src/createFieldProps.js
+++ src/createFieldProps.js
@@ -22,6 +22,13 @@ const processProps = (props, _value) => {
       value: _value
     }
   }
+  if (type === 'select-multiple') {
+    return {
+      ...rest,
+      type,
+      value: value || []
+    }
+  }
   return props
 }
 
