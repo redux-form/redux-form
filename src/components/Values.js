8d0abe2531d16419af5b6bb55498b3ccbe5ab3a4
Erik Rasmussen <rasmussenerik@gmail.com>
2016-05-23 18:29:07 +0200

    added format param to Values

--- src/components/Values.js
+++ src/components/Values.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { values as valuesDecorator } from 'redux-form'
 import Code from './Code'
 
-const Values = ({ form }) => {
+const Values = ({ form, format = values => JSON.stringify(values, null, 2) }) => {
   const decorator = valuesDecorator({ form })
   const component = ({ values }) =>
     (
       <div>
         <h2>Values</h2>
-        <Code source={JSON.stringify(values, null, 2)}/>
+        <Code source={format(values)}/>
       </div>
     )
   const Decorated = decorator(component)
