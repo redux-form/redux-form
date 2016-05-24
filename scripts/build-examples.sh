604b261ca3346422b1c3617d2b0fb3fbd037b715
Erik Rasmussen <rasmussenerik@gmail.com>
2016-05-24 16:43:08 +0200

    build examples bash script

---
+++ scripts/build-examples.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+rebuild=false
+while test $# -gt 0; do
+  case "$1" in
+    --rebuild)
+      rebuild=true
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if $rebuild
+then
+  echo 'Rebuilding'
+fi
+
+cd examples
+for example in `ls -d */`
+do
+  echo $example
+  cd $example
+  if $rebuild
+  then
+    rm -rf node_modules
+  else
+    rm -rf node_modules/redux-form
+  fi
+  npm install
+  cd ..
+done
+
+
