9412780d93e1b97c233bb7e3af59c73d57a5bf80
Erik Rasmussen <rasmussenerik@gmail.com>
2016-08-01 20:09:37 +0200

    exported selectors

--- src/createAll.js
+++ src/createAll.js
@@ -4,6 +4,11 @@ import createField from './Field'
 import createFieldArray from './FieldArray'
 import createFormValueSelector from './formValueSelector'
 import createValues from './values'
+import createGetFormValues from './selectors/getFormValues'
+import createIsDirty from './selectors/isDirty'
+import createIsInvalid from './selectors/isInvalid'
+import createIsPristine from './selectors/isPristine'
+import createIsValid from './selectors/isValid'
 import SubmissionError from './SubmissionError'
 import propTypes from './propTypes'
 import * as actions from './actions'
@@ -16,6 +21,11 @@ const createAll = structure => ({
   Field: createField(structure),
   FieldArray: createFieldArray(structure),
   formValueSelector: createFormValueSelector(structure),
+  getFormValues: createGetFormValues(structure),
+  isDirty: createIsDirty(structure),
+  isInvalid: createIsInvalid(structure),
+  isPristine: createIsPristine(structure),
+  isValid: createIsValid(structure),
   propTypes,
   reduxForm: createReduxForm(structure),
   reducer: createReducer(structure),
