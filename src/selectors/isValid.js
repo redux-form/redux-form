9cf6a711efd32a0beeb63f13728755162025b5df
Erik Rasmussen <rasmussenerik@gmail.com>
2016-08-24 11:10:55 +0200

    No longer need hasErrors. Consolidated hasError() logic from #1508.

--- src/selectors/isValid.js
+++ src/selectors/isValid.js
@@ -1,12 +1,7 @@
-import createHasErrors from '../hasErrors'
 import createHasError from '../hasError'
-import plain from '../structure/plain'
-
-const plainHasErrors = createHasErrors(plain)
 
 const createIsValid = structure => {
   const { getIn } = structure
-  const hasErrors = createHasErrors(structure)
   const hasError = createHasError(structure)
   return (form, getFormState = state => getIn(state, 'form')) =>
     state => {
@@ -16,21 +11,14 @@ const createIsValid = structure => {
         return false
       }
       const syncErrors = getIn(formState, `${form}.syncErrors`)
-      if(plainHasErrors(syncErrors)) {
-        return false
-      }
       const asyncErrors = getIn(formState, `${form}.asyncErrors`)
-      if(hasErrors(asyncErrors)) {
-        return false
-      }
       const submitErrors = getIn(formState, `${form}.submitErrors`)
-      if(hasErrors(submitErrors)) {
-        return false
+      if(!syncErrors && !asyncErrors && !submitErrors) {
+        return true
       }
+
       const registeredFields = getIn(formState, `${form}.registeredFields`) || []
-      const hasFieldWithError = registeredFields &&
-        registeredFields.some(field => hasError(field, syncErrors, asyncErrors, submitErrors))
-      return !hasFieldWithError
+      return !registeredFields.some(field => hasError(field, syncErrors, asyncErrors, submitErrors))
     }
 }
 
