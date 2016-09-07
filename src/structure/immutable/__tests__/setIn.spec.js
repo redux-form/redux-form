0e45ea89a01e71979bc093e9f0b2d222a1da7fd2
Aren Blondahl <rean1985@gmail.com>
2016-09-07 03:08:38 -0600

    Added more comprehensive unit tests for immutable/setIn

--- src/structure/immutable/__tests__/setIn.spec.js
+++ src/structure/immutable/__tests__/setIn.spec.js
@@ -118,4 +118,148 @@ describe('structure.immutable.setIn', () => {
     let e = d.get('e')
     expect(e).toExist('e missing').toEqual('success')
   })
+  it('should update existing Map', () => {
+    let initial = fromJS({
+      a: {
+        b: { c: 'one' }
+      }
+    })
+
+    let result = setIn(initial, 'a.b.c', 'two')
+    
+    let a = result.get('a')
+    expect(a).toExist('a missing')
+
+    let b = a.get('b')
+    expect(b).toExist('b missing')
+
+    let c = b.get('c')
+    expect(c).toEqual('two')
+  })
+  it('should update existing List', () => {
+    let initial = fromJS({
+      a: {
+        b: [ { c: 'one' } ]
+      }
+    })
+
+    let result = setIn(initial, 'a.b[0].c', 'two')
+    
+    let a = result.get('a')
+    expect(a).toExist('a missing')
+
+    let b = a.get('b')
+    expect(b).toExist('b missing')
+
+    let b0 = b.get(0)
+    expect(b0).toExist()
+
+    let b0c = b0.get('c')
+    expect(b0c).toEqual('two')
+  })
+  it('should not break existing Map', () => {
+    let initial = fromJS({
+      a: {
+        b: { c: 'one' }
+      }
+    })
+
+    let result = setIn(initial, 'a.b.d', 'two')
+    
+    let a = result.get('a')
+    expect(a).toExist('a missing')
+
+    let b = a.get('b')
+    expect(b).toExist('b missing')
+
+    let c = b.get('c')
+    expect(c).toEqual('one')
+
+    let d = b.get('d')
+    expect(d).toEqual('two')
+  })
+  it('should not break existing List', () => {
+    let initial = fromJS({
+      a: {
+        b: [
+          { c: 'one' },
+          { c: 'two' }
+        ]
+      }
+    })
+
+    let result = setIn(initial, 'a.b[0].c', 'changed')
+    
+    let a = result.get('a')
+    expect(a).toExist('a missing')
+
+    let b = a.get('b')
+    expect(b).toExist('b missing')
+
+    let b0 = b.get(0)
+    expect(b0).toExist()
+
+    let b0c = b0.get('c')
+    expect(b0c).toEqual('changed')
+
+    let b1 = b.get(1)
+    expect(b1).toExist()
+
+    let b1c = b1.get('c')
+    expect(b1c).toEqual('two')
+  })
+  it('should add to an existing List', () => {
+    let initial = fromJS({
+      a: {
+        b: [
+          { c: 'one' },
+          { c: 'two' }
+        ]
+      }
+    })
+
+    let result = setIn(initial, 'a.b[2].c', 'three')
+    
+    let a = result.get('a')
+    expect(a).toExist('a missing')
+
+    let b = a.get('b')
+    expect(b).toExist('b missing')
+
+    let b0 = b.get(0)
+    expect(b0).toExist()
+
+    let b0c = b0.get('c')
+    expect(b0c).toEqual('one')
+
+    let b1 = b.get(1)
+    expect(b1).toExist()
+
+    let b1c = b1.get('c')
+    expect(b1c).toEqual('two')
+
+    let b2 = b.get(2)
+    expect(b2).toExist()
+
+    let b2c = b2.get('c')
+    expect(b2c).toEqual('three')
+  })
+  it('should set a value directly on new list', () => {
+    let result = setIn(new Map(), 'a.b[2]', 'three')
+    
+    let a = result.get('a')
+    expect(a).toExist('a missing')
+
+    let b = a.get('b')
+    expect(b).toExist('b missing')
+
+    let b0 = b.get(0)
+    expect(b0).toEqual(undefined)
+
+    let b1 = b.get(1)
+    expect(b1).toEqual(undefined)
+
+    let b2 = b.get(2)
+    expect(b2).toEqual('three')
+  })
 })
