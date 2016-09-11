06f08dd717024095fdc0f497168efc47a2c4c24c
Jeow Li Huan <huan086@gmail.com>
2016-09-11 13:03:57 +0800

    Rewrite recursive rest spread getInPath into simple for loop for 10x speed improvement.

--- src/structure/plain/getIn.js
+++ src/structure/plain/getIn.js
@@ -1,13 +1,5 @@
 import { toPath } from 'lodash'
 
-const getInWithPath = (state, first, ...rest) => {
-  if(!state) {
-    return state
-  }
-  const next = state[first]
-  return rest.length ? getInWithPath(next, ...rest) : next
-}
-
 const getIn = (state, field) => {
   if (!state) {
     return state
@@ -15,27 +7,14 @@ const getIn = (state, field) => {
 
   const path = toPath(field)
   const length = path.length
-
-  if (length > 3) {
-    return getInWithPath(state, ...path)
-  }
-
-  let result = state
-  if (!length || !result) {
+  if (!length) {
     return undefined
   }
-
-  result = result[path[0]]
-  if (length === 1 || !result) {
-    return result
+  
+  let result = state
+  for (let i = 0; i < length && !!result; ++i) {
+    result = result[path[i]]
   }
-
-  result = result[path[1]]
-  if (length === 2 || !result) {
-    return result
-  }
-
-  result = result[path[2]]
 
   return result
 }
