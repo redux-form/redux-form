a4c1d9f0382d434402b55f5175c17b868fc6b2f5
Benoit Bénézech <benoit.benezech@gmail.com>
2016-09-15 10:06:30 +0200

    Improve dirty handling for check-boxes (#1762)
    `dirty(undefined, false)` should be `false`. It was `true`.
    
    Intuitively, clicking twice on a check-box should return it to
    pristine, which is false atm (unless the checkbox is initialized with
    `false`).
    
    Same behavior as empty and undefined for a string.
    
    Also decreases code complexity. If both value and initial are falsey,
    return âequal trueâ.
    
    Comes with tests, both immutable and plain structures are covered.
    
    Iâd call it a safe merge and would be very pleased to see it shipped.
    :hatching_chick:

--- src/structure/immutable/deepEqual.js
+++ src/structure/immutable/deepEqual.js
@@ -4,8 +4,8 @@ import { isEqualWith } from 'lodash'
 
 const customizer = (obj, other) => {
   if (obj == other) return true
-  if (obj == null && other === '') return true
-  if (obj === '' && other == null) return true
+  if ((obj == null || obj === '' || obj === false) &&
+    (other == null || other === '' || other === false)) return true
 
   if (Iterable.isIterable(obj) && Iterable.isIterable(other)) {
     return obj.count() === other.count() && obj.every((value, key) => {
