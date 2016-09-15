d3f102b03d3ab0295c9995ade846f1d54270eb7e
Erik Rasmussen <rasmussenerik@gmail.com>
2016-09-15 09:46:26 +0200

    Bound change and blur to dispatch #1696

--- src/reduxForm.js
+++ src/reduxForm.js
@@ -461,8 +461,10 @@ const createReduxForm =
             const computedActions = {
               ...connectedFormACs,
               ...boundArrayACs,
-              blur: boundBlur,
-              change: boundChange,
+              ...bindActionCreators({
+                blur: boundBlur,
+                change: boundChange
+              }, dispatch),
               array: connectedArrayACs,
               focus: boundFocus,
               dispatch
