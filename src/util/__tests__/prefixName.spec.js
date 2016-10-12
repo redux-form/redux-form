cbe8e5c418af95b86b1c5cb01a0cd5c87e058786
Simon Rasmussen <zetlon@gmail.com>
2016-10-13 01:22:17 +0800

    Added prefixName tests

---
+++ src/util/__tests__/prefixName.spec.js
@@ -0,0 +1,22 @@
+import expect from 'expect'
+import prefixName from '../prefixName'
+
+describe('prefixName', () => {
+  it('should concat sectionPrefix and name', () => {
+    const context = {
+      _reduxForm: {
+        sectionPrefix: 'foo'
+      }
+    }
+    expect(prefixName(context,'bar')).toBe('foo.bar')
+  })
+
+  it('should ignore empty sectionPrefix', () => {
+    const context = {
+      _reduxForm: {
+        sectionPrefix: undefined
+      }
+    }
+    expect(prefixName(context, 'bar')).toBe('bar')
+  })
+})
