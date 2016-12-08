cae8d906284e7e447f73ad503a7d9be0acde0183
Erik Rasmussen <rasmussenerik@gmail.com>
2016-12-08 17:08:25 +0100

    Added example

---
+++ examples/fieldLevelValidation/README.md
@@ -0,0 +1,10 @@
+# Field Level Validation Example
+
+## To run locally
+
+```
+npm install
+npm start
+```
+
+Then open [`http://localhost:3030/`](http://localhost:3030/).
---
+++ examples/fieldLevelValidation/src/index.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
+import { reducer as reduxFormReducer } from 'redux-form'
+import { App, Code, Markdown, Values, generateExampleBreadcrumbs } from 'redux-form-website-template'
+
+const dest = document.getElementById('content')
+const reducer = combineReducers({
+  form: reduxFormReducer // mounted under "form"
+})
+const store =
+  (window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore)(reducer)
+
+const showResults = values =>
+  new Promise(resolve => {
+    setTimeout(() => {  // simulate server latency
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+      resolve()
+    }, 500)
+  })
+
+let render = () => {
+  const FieldLevelValidationForm = require('./FieldLevelValidationForm').default
+  const readme = require('./FieldLevelValidation.md')
+  const raw = require('!!raw!./FieldLevelValidationForm')
+  ReactDOM.render(
+    <Provider store={store}>
+      <App
+        /**
+         * This <App/> component only provides the site wrapper.
+         * Remove it on your dev server if you wish. It will not affect the functionality.
+         */
+        version="6.2.1"
+        path="/examples/fieldLevelValidation"
+        breadcrumbs={generateExampleBreadcrumbs('fieldLevelValidation',
+          'Field-Level Validation Example', '6.2.1')}>
+
+        <Markdown content={readme}/>
+
+        <h2>Form</h2>
+
+        <FieldLevelValidationForm onSubmit={showResults}/>
+
+        <Values form="fieldLevelValidation"/>
+
+        <h2>Code</h2>
+
+        <h3>FieldLevelValidationForm.js</h3>
+
+        <Code source={raw}/>
+
+      </App>
+    </Provider>,
+    dest
+  )
+}
+
+if (module.hot) {
+  // Support hot reloading of components
+  // and display an overlay for runtime errors
+  const renderApp = render
+  const renderError = (error) => {
+    const RedBox = require('redbox-react')
+    ReactDOM.render(
+      <RedBox error={error} className="redbox"/>,
+      dest
+    )
+  }
+  render = () => {
+    try {
+      renderApp()
+    } catch (error) {
+      renderError(error)
+    }
+  }
+  const rerender = () => {
+    setTimeout(render)
+  }
+  module.hot.accept('./FieldLevelValidationForm', rerender)
+  module.hot.accept('./FieldLevelValidation.md', rerender)
+  module.hot.accept('!!raw!./FieldLevelValidationForm', rerender)
+}
+
+render()
