113781d056a117cee3dccf14f5ccbeec62ecb79e
Erik Rasmussen <rasmussenerik@gmail.com>
2016-12-29 22:36:17 +0100

    Linting for #2339

--- examples/normalizing/src/normalizePhone.js
+++ examples/normalizing/src/normalizePhone.js
@@ -1,17 +1,16 @@
 const normalizePhone = (value) => {
   if (!value) {
-    return value;
+    return value
   }
 
-  const onlyNums = value.replace(/[^\d]/g, '');
-
+  const onlyNums = value.replace(/[^\d]/g, '')
   if (onlyNums.length <= 3) {
-    return onlyNums;
+    return onlyNums
   }
-  if (onlyNums.length <= 6) {
-    return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3)}`;
+  if (onlyNums.length <= 7) {
+    return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3)}`
   }
-  return `(${onlyNums.slice(0, 3)}) ${onlyNums.slice(3, 6)}-${onlyNums.slice(6, 10)}`;
-};
+  return `${onlyNums.slice(0, 3)}-${onlyNums.slice(3, 6)}-${onlyNums.slice(6, 10)}`
+}
 
-export default normalizePhone;
+export default normalizePhone
