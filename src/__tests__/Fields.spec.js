redux-form/redux-form

commit 92399dd59be736b381f79ec66e25920c74363658

Bind handlers to components (#2161)
* ConnectedField: Bind handlers to component

Makes functions preserve their identity, thus not causing
unnecessary re-renders.

* events: Clean up unused functions
onChangeValue: Separate logic, write test

* createFieldProps: Fix failing tests

Fixed all instances where the function was creating the callbacks.

* onChangeValue: Fix bounding of parse/normalize

I overlooked the part where parse/normalize are bound with a name.

* ConnectedFields: Fix handler functions, general cleanup

Also adjusted tests, cleaned up unused variables.

* onChangeValue: Fix eslint

* createFieldProps: Fix merge issues

* eventMocks: Write tests

* eventMocks: Removed where redundant

* Field: Fix code coverage

* Fields: Fix code coverage

* eventMocks: Write explicit tests

* eventMocks: Fix lint

* Field: Increase branch cov

src/ConnectedFields.js
@@ -2,6 +2,7 @@ import { Component, PropTypes, createElement } from 'react'
 import { connect } from 'react-redux'
 import createFieldProps from './createFieldProps'
 import plain from './structure/plain'
+import onChangeValue from './events/onChangeValue'
 
 const propsToNotUpdateFor = [
   '_reduxForm'
@@ -24,6 +25,26 @@ const createConnectedFields = ({ deepEqual, getIn }) => {
   }
 
   class ConnectedFields extends Component {
+    constructor(props) {
+      super(props)
+
+      this.handleChange = this.handleChange.bind(this)
+      this.handleFocus = this.handleFocus.bind(this)
+      this.handleBlur = this.handleBlur.bind(this)
+
+      this.onChangeFns = Object.keys(props._fields).reduce((acc, name) => ({
+        ...acc, [name]: event => this.handleChange(name, event)
+      }), {})
+
+      this.onFocusFns = Object.keys(props._fields).reduce((acc, name) => ({
+        ...acc, [name]: () => this.handleFocus(name)
+      }), {})
+
+      this.onBlurFns = Object.keys(props._fields).reduce((acc, name) => ({
+        ...acc, [name]: event => this.handleBlur(name, event)
+      }), {})
+    }
+
     shouldComponentUpdate(nextProps) {
       const nextPropsKeys = Object.keys(nextProps)
       const thisPropsKeys = Object.keys(this.props)
@@ -47,9 +68,34 @@ const createConnectedFields = ({ deepEqual, getIn }) => {
       return this.refs.renderedComponent
     }
 
+    handleChange(name, event) {
+      const { dispatch, parse, normalize, _reduxForm } = this.props
+      const value = onChangeValue(event, { name, parse, normalize })
+
+      dispatch(_reduxForm.change(name, value))
+    }
+
+    handleFocus(name) {
+      const { dispatch, _reduxForm } = this.props
+      dispatch(_reduxForm.focus(name))
+    }
+
+    handleBlur(name, event) {
+      const { dispatch, parse, normalize, _reduxForm } = this.props
+      const value = onChangeValue(event, { name, parse, normalize })
+
+      // dispatch blur action
+      dispatch(_reduxForm.blur(name, value))
+
+      // call post-blur callback
+      if (_reduxForm.asyncValidate) {
+        _reduxForm.asyncValidate(name, value)
+      }
+    }
+
     render() {
       const { component, withRef, _fields, _reduxForm, ...rest } = this.props
-      const { asyncValidate, blur, change, focus, sectionPrefix } = _reduxForm
+      const { sectionPrefix } = _reduxForm
       const { custom, ...props } = Object.keys(_fields).reduce((accumulator, name) => {
         const connectedProps = _fields[ name ]
         const { custom, ...fieldProps } = createFieldProps(getIn,
@@ -57,11 +103,10 @@ const createConnectedFields = ({ deepEqual, getIn }) => {
           {
             ...connectedProps,
             ...rest,
-            blur,
-            change,
-            focus
-          },
-          asyncValidate
+            onBlur: this.onBlurFns[name],
+            onChange: this.onChangeFns[name],
+            onFocus: this.onFocusFns[name]
+          }
         )
         accumulator.custom = custom
         const fieldName = sectionPrefix ? name.replace(`${sectionPrefix}.`, '') : name

src/__tests__/Fields.spec.js
@@ -975,6 +975,40 @@ const describeFields = (name, structure, combineReducers, expect) => {
       expect(input.calls[ 1 ].arguments[ 0 ].name.input.value).toBe('redux form rocks')
     })
 
+    it('should handle on focus', () => {
+      const store = makeStore({
+        testForm: {
+          values: {
+            name: 'redux form'
+          }
+        }
+      })
+      const input = createSpy(props => <input {...props.input}/>).andCallThrough()
+      class Form extends Component {
+        render() {
+          return (
+            <div>
+              <Fields names={[ 'name' ]} component={input}/>
+            </div>
+          )
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+      expect(input.calls.length).toBe(1)
+      expect(input.calls[ 0 ].arguments[ 0 ].name.meta.visited).toBe(false)
+
+      input.calls[ 0 ].arguments[ 0 ].name.input.onFocus()
+
+      expect(input.calls.length).toBe(2)
+      expect(input.calls[ 1 ].arguments[ 0 ].name.meta.visited).toBe(true)
+    })
+
     it('should parse and format to maintain different type in store', () => {
       const store = makeStore({
         testForm: {

src/ConnectedField.js
@@ -2,6 +2,8 @@ import { Component, PropTypes, createElement } from 'react'
 import { connect } from 'react-redux'
 import createFieldProps from './createFieldProps'
 import plain from './structure/plain'
+import onChangeValue from './events/onChangeValue'
+import { dataKey } from './util/eventConsts'
 
 const propsToNotUpdateFor = [
   '_reduxForm'
@@ -24,6 +26,16 @@ const createConnectedField = ({ deepEqual, getIn }) => {
   }
 
   class ConnectedField extends Component {
+    constructor(props) {
+      super(props)
+
+      this.handleChange = this.handleChange.bind(this)
+      this.handleFocus = this.handleFocus.bind(this)
+      this.handleBlur = this.handleBlur.bind(this)
+      this.handleDragStart = this.handleDragStart.bind(this)
+      this.handleDrop = this.handleDrop.bind(this)
+    }
+
     shouldComponentUpdate(nextProps) {
       const nextPropsKeys = Object.keys(nextProps)
       const thisPropsKeys = Object.keys(this.props)
@@ -44,19 +56,54 @@ const createConnectedField = ({ deepEqual, getIn }) => {
       return this.refs.renderedComponent
     }
 
+    handleChange(event) {
+      const { name, dispatch, parse, normalize, _reduxForm } = this.props
+      const value = onChangeValue(event, { name, parse, normalize })
+
+      dispatch(_reduxForm.change(name, value))
+    }
+
+    handleFocus() {
+      const { name, dispatch, _reduxForm } = this.props
+      dispatch(_reduxForm.focus(name))
+    }
+
+    handleBlur(event) {
+      const { name, dispatch, parse, normalize, _reduxForm } = this.props
+      const value = onChangeValue(event, { name, parse, normalize })
+
+      // dispatch blur action
+      dispatch(_reduxForm.blur(name, value))
+
+      // call post-blur callback
+      if (_reduxForm.asyncValidate) {
+        _reduxForm.asyncValidate(name, value)
+      }
+    }
+
+    handleDragStart(event) {
+      const { value } = this.props
+      event.dataTransfer.setData(dataKey, value == null ? '' : value)
+    }
+
+    handleDrop(event) {
+      const { name, dispatch, _reduxForm } = this.props
+      dispatch(_reduxForm.change(name, event.dataTransfer.getData(dataKey)))
+      event.preventDefault()
+    }
+
     render() {
-      const { component, withRef, name, _reduxForm, ...rest } = this.props
-      const { asyncValidate, blur, change, focus } = _reduxForm
+      const { component, withRef, name, ...rest } = this.props
       const { custom, ...props } = createFieldProps(getIn,
         name,
         {
           ...rest,
-          name,
-          blur,
-          change,
-          focus
-        },
-        asyncValidate
+          onBlur: this.handleBlur,
+          onChange: this.handleChange,
+          onDrop: this.handleDrop,
+          onDragStart: this.handleDragStart,
+          onFocus: this.handleFocus
+        }
       )
       if (withRef) {
         custom.ref = 'renderedComponent'

src/__tests__/Field.spec.js
@@ -14,6 +14,8 @@ import plainExpectations from '../structure/plain/expectations'
 import immutable from '../structure/immutable'
 import immutableExpectations from '../structure/immutable/expectations'
 import addExpectations from './addExpectations'
+import { dragStartMock, dropMock } from '../util/eventMocks'
+import { dataKey } from '../util/eventConsts'
 
 const describeField = (name, structure, combineReducers, expect) => {
   const reduxForm = createReduxForm(structure)
@@ -849,6 +851,168 @@ const describeField = (name, structure, combineReducers, expect) => {
       expect(normalize.calls.length).toBe(1)
 
       expect(renderUsername.calls[ 1 ].arguments[ 0 ].input.value).toBe('erikras')
+    })
+
+    it('should call asyncValidate function on blur', () => {
+      const store = makeStore({
+        testForm: {
+          values: {
+            title: 'Redux Form',
+            author: 'Erik Rasmussen',
+            username: 'oldusername'
+          }
+        }
+      })
+      const renderUsername = createSpy(props => <input {...props.input}/>).andCallThrough()
+      class Form extends Component {
+        render() {
+          return (
+            <div>
+              <Field name="title" component="input"/>
+              <Field name="author" component="input"/>
+              <Field name="username" component={renderUsername}/>
+            </div>
+          )
+        }
+      }
+      const asyncValidate = createSpy(() => new Promise(resolve => resolve())).andCallThrough()
+      const TestForm = reduxForm({ form: 'testForm', asyncValidate })(Form)
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+      renderUsername.calls[ 0 ].arguments[ 0 ].input.onBlur('ERIKRAS')
+
+      expect(asyncValidate).toHaveBeenCalled()
+    })
+
+    it('should call handle on focus', () => {
+      const store = makeStore({
+        testForm: {
+          values: {
+            title: 'Redux Form'
+          }
+        }
+      })
+      const renderTitle = createSpy(props => <input {...props.input}/>).andCallThrough()
+      class Form extends Component {
+        render() {
+          return (
+            <Field name="title" component={renderTitle} />
+          )
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+
+      expect(renderTitle.calls[ 0 ].arguments[ 0 ].meta.visited).toBe(false)
+      renderTitle.calls[ 0 ].arguments[ 0 ].input.onFocus()
+      expect(renderTitle.calls[ 1 ].arguments[ 0 ].meta.visited).toBe(true)
+    })
+
+    it('should call handle on drag start with value', () => {
+      const store = makeStore({
+        testForm: {
+          values: {
+            title: 'Redux Form'
+          }
+        }
+      })
+      const renderTitle = createSpy(props => <input {...props.input}/>).andCallThrough()
+      const dragSpy = createSpy((key, val) => val).andCallThrough()
+      const event = dragStartMock(dragSpy)
+      class Form extends Component {
+        render() {
+          return (
+            <Field name="title" component={renderTitle} />
+          )
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+      expect(dragSpy).toNotHaveBeenCalled()
+      renderTitle.calls[ 0 ].arguments[ 0 ].input.onDragStart(event)
+      expect(dragSpy)
+        .toHaveBeenCalled()
+        .toHaveBeenCalledWith(dataKey, 'Redux Form')
+    })
+
+    it('should call handle on drag start without value', () => {
+      const store = makeStore({
+        testForm: {
+          values: {
+            title: null
+          }
+        }
+      })
+      const renderTitle = createSpy(props => <input {...props.input}/>).andCallThrough()
+      const dragSpy = createSpy((key, val) => val).andCallThrough()
+      const event = dragStartMock(dragSpy)
+      class Form extends Component {
+        render() {
+          return (
+            <Field name="title" component={renderTitle} />
+          )
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+      expect(dragSpy).toNotHaveBeenCalled()
+      renderTitle.calls[ 0 ].arguments[ 0 ].input.onDragStart(event)
+      expect(dragSpy)
+        .toHaveBeenCalled()
+        .toHaveBeenCalledWith(dataKey, '')
+    })
+
+    it('should call handle on drop', () => {
+      const store = makeStore({
+        testForm: {
+          values: {
+            title: 'Redux Form'
+          }
+        }
+      })
+      const renderTitle = createSpy(props => <input {...props.input}/>).andCallThrough()
+      const dropSpy = createSpy(key => key).andCallThrough()
+      const event = dropMock(dropSpy)
+      event.preventDefault = createSpy(event.preventDefault)
+      class Form extends Component {
+        render() {
+          return (
+            <Field name="title" component={renderTitle} />
+          )
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+      expect(dropSpy).toNotHaveBeenCalled()
+      renderTitle.calls[ 0 ].arguments[ 0 ].input.onDrop(event)
+      expect(event.preventDefault).toHaveBeenCalled()
+      expect(dropSpy)
+        .toHaveBeenCalled()
+        .toHaveBeenCalledWith(dataKey)
     })
 
     it('should call format function on first render', () => {

src/__tests__/createFieldProps.spec.js
@@ -22,61 +22,48 @@ const describeCreateFieldProps = (name, structure, expect) => {
     })
 
     it('should provide onBlur', () => {
-      const blur = createSpy()
-      const dispatch = createSpy()
-      const normalize = createSpy((name, value) => value).andCallThrough()
-      expect(blur).toNotHaveBeenCalled()
-      const result = createFieldProps(getIn, 'foo', { value: 'bar', blur, dispatch, normalize })
+      const onBlur = createSpy()
+      expect(onBlur).toNotHaveBeenCalled()
+      const result = createFieldProps(getIn, 'foo', { value: 'bar', onBlur })
       expect(result.input.onBlur).toBeA('function')
-      expect(blur).toNotHaveBeenCalled()
-      expect(dispatch).toNotHaveBeenCalled()
+      expect(onBlur).toNotHaveBeenCalled()
       result.input.onBlur('rabbit')
-      expect(normalize).toHaveBeenCalled()
-      expect(blur)
+      expect(onBlur)
         .toHaveBeenCalled()
-        .toHaveBeenCalledWith('foo', 'rabbit')
-      expect(dispatch).toHaveBeenCalled()
+        .toHaveBeenCalledWith('rabbit')
     })
 
     it('should provide onChange', () => {
-      const change = createSpy()
-      const dispatch = createSpy()
-      const normalize = createSpy((name, value) => value).andCallThrough()
-      expect(change).toNotHaveBeenCalled()
-      const result = createFieldProps(getIn, 'foo', { value: 'bar', change, dispatch, normalize })
+      const onChange = createSpy()
+      expect(onChange).toNotHaveBeenCalled()
+      const result = createFieldProps(getIn, 'foo', { value: 'bar', onChange })
       expect(result.input.onChange).toBeA('function')
-      expect(change).toNotHaveBeenCalled()
-      expect(dispatch).toNotHaveBeenCalled()
+      expect(onChange).toNotHaveBeenCalled()
       result.input.onChange('rabbit')
-      expect(normalize).toHaveBeenCalled()
-      expect(change)
+      expect(onChange)
         .toHaveBeenCalled()
-        .toHaveBeenCalledWith('foo', 'rabbit')
-      expect(dispatch).toHaveBeenCalled()
+        .toHaveBeenCalledWith('rabbit')
     })
 
     it('should provide onFocus', () => {
-      const focus = createSpy()
-      const dispatch = createSpy()
-      expect(focus).toNotHaveBeenCalled()
-      const result = createFieldProps(getIn, 'foo', { value: 'bar', dispatch, focus })
+      const onFocus = createSpy()
+      expect(onFocus).toNotHaveBeenCalled()
+      const result = createFieldProps(getIn, 'foo', { value: 'bar', onFocus })
       expect(result.input.onFocus).toBeA('function')
-      expect(focus).toNotHaveBeenCalled()
-      expect(dispatch).toNotHaveBeenCalled()
+      expect(onFocus).toNotHaveBeenCalled()
       result.input.onFocus('rabbit')
-      expect(focus)
-        .toHaveBeenCalled()
-        .toHaveBeenCalledWith('foo')
-      expect(dispatch).toHaveBeenCalled()
+      expect(onFocus).toHaveBeenCalled()
     })
 
     it('should provide onDragStart', () => {
-      const result = createFieldProps(getIn, 'foo', { value: 'bar' })
+      const onDragStart = createSpy()
+      const result = createFieldProps(getIn, 'foo', { value: 'bar', onDragStart })
       expect(result.input.onDragStart).toBeA('function')
     })
 
     it('should provide onDrop', () => {
-      const result = createFieldProps(getIn, 'foo', { value: 'bar' })
+      const onDrop = createSpy()
+      const result = createFieldProps(getIn, 'foo', { value: 'bar', onDrop })
       expect(result.input.onDrop).toBeA('function')
     })
 

src/createFieldProps.js
@@ -1,10 +1,3 @@
-import createOnBlur from './events/createOnBlur'
-import createOnChange from './events/createOnChange'
-import createOnDragStart from './events/createOnDragStart'
-import createOnDrop from './events/createOnDrop'
-import createOnFocus from './events/createOnFocus'
-import { noop } from 'lodash'
-
 const processProps = (type, props, _value) => {
   const { value } = props
   if (type === 'checkbox') {
@@ -37,19 +30,12 @@ const processProps = (type, props, _value) => {
 
 const createFieldProps = (getIn, name,
   {
-    asyncError, asyncValidating, blur, change, dirty, dispatch, focus, format,
-    normalize, parse, pristine, props, state, submitError, submitting, value,
+    asyncError, asyncValidating, onBlur, onChange, onDrop, onDragStart, dirty, dispatch, onFocus, format,
+    pristine, props, state, submitError, submitting, value,
     _value, syncError, syncWarning, ...custom
-  }, asyncValidate = noop) => {
+  }) => {
   const error = syncError || asyncError || submitError
   const warning = syncWarning
-  const boundParse = parse && (value => parse(value, name))
-  const boundNormalize = normalize && (value => normalize(name, value))
-  const boundChange = value => dispatch(change(name, value))
-  const onChange = createOnChange(boundChange, {
-    normalize: boundNormalize,
-    parse: boundParse
-  })
 
   const formatFieldValue = (value, format) => {
     if (format === null) {
@@ -64,15 +50,11 @@ const createFieldProps = (getIn, name,
   return {
     input: processProps(custom.type, {
       name,
-      onBlur: createOnBlur(value => dispatch(blur(name, value)), {
-        normalize: boundNormalize,
-        parse: boundParse,
-        after: asyncValidate.bind(null, name)
-      }),
+      onBlur,
       onChange,
-      onDragStart: createOnDragStart(name, formattedFieldValue),
-      onDrop: createOnDrop(name, boundChange),
-      onFocus: createOnFocus(name, () => dispatch(focus(name))),
+      onDragStart,
+      onDrop,
+      onFocus,
       value: formattedFieldValue
     }, _value),
     meta: {

src/events/__tests__/createOnBlur.spec.js
@@ -1,63 +1,0 @@
-import expect, { createSpy } from 'expect'
-import createOnBlur from '../createOnBlur'
-
-describe('createOnBlur', () => {
-  it('should return a function', () => {
-    expect(createOnBlur())
-      .toExist()
-      .toBeA('function')
-  })
-
-  it('should parse the value before dispatching action', () => {
-    const blur = createSpy()
-    const parse = createSpy(value => `parsed-${value}`).andCallThrough()
-    createOnBlur(blur, { parse })('bar')
-    expect(parse)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('bar')
-    expect(blur)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('parsed-bar')
-  })
-  
-  it('should normalize the value before dispatching action', () => {
-    const blur = createSpy()
-    const normalize = createSpy(value => `normalized-${value}`).andCallThrough()
-    createOnBlur(blur, { normalize })('bar')
-    expect(normalize)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('bar')
-    expect(blur)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('normalized-bar')
-  })
-    
-  it('should parse before normalize', () => {
-    const blur = createSpy()
-    const parse = createSpy(value => `parsed-${value}`).andCallThrough()
-    const normalize = createSpy(value => `normalized-${value}`).andCallThrough()
-    createOnBlur(blur, { normalize, parse })('bar')
-    expect(parse)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('bar')
-    expect(normalize)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('parsed-bar')
-    expect(blur)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('normalized-parsed-bar')
-  })
-  
-  it('should call blur then after', () => {
-    const blur = createSpy()
-    const parse = createSpy(value => `parsed-${value}`).andCallThrough()
-    const normalize = createSpy(value => `normalized-${value}`).andCallThrough()
-    const after = createSpy()
-    createOnBlur(blur, { parse, normalize, after })('bar')
-    expect(blur).toHaveBeenCalled()
-    expect(normalize).toHaveBeenCalled()
-    expect(after)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('normalized-parsed-bar')
-  })
-})

src/events/__tests__/createOnChange.spec.js
@@ -1,50 +1,0 @@
-import expect, { createSpy } from 'expect'
-import createOnChange from '../createOnChange'
-
-describe('createOnChange', () => {
-  it('should return a function', () => {
-    expect(createOnChange())
-      .toExist()
-      .toBeA('function')
-  })
-
-  it('should parse the value before dispatching action', () => {
-    const change = createSpy()
-    const parse = createSpy(value => `parsed-${value}`).andCallThrough()
-    createOnChange(change, { parse })('bar')
-    expect(parse)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('bar')
-    expect(change)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('parsed-bar')
-  })
-
-  it('should normalize the value before dispatching action', () => {
-    const change = createSpy()
-    const normalize = createSpy(value => `normalized-${value}`).andCallThrough()
-    createOnChange(change, { normalize })('bar')
-    expect(normalize)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('bar')
-    expect(change)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('normalized-bar')
-  })
-
-  it('should parse before normalize', () => {
-    const change = createSpy()
-    const parse = createSpy(value => `parsed-${value}`).andCallThrough()
-    const normalize = createSpy(value => `normalized-${value}`).andCallThrough()
-    createOnChange(change, { normalize, parse })('bar')
-    expect(parse)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('bar')
-    expect(normalize)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('parsed-bar')
-    expect(change)
-      .toHaveBeenCalled()
-      .toHaveBeenCalledWith('normalized-parsed-bar')
-  })
-})

commit 34657e5a0d912e1efd2f671181e65ca689d6cafa

Update ConnectedFields cached handlers when names prop changes. Fixes bug introduced by #2161.

src/ConnectedFields.js
@@ -8,7 +8,7 @@ const propsToNotUpdateFor = [
   '_reduxForm'
 ]
 
-const createConnectedFields = ({ deepEqual, getIn, toJS }) => {
+const createConnectedFields = ({ deepEqual, getIn, toJS, size }) => {
 
   const getSyncError = (syncErrors, name) => {
     const error = plain.getIn(syncErrors, name)
@@ -32,19 +32,45 @@ const createConnectedFields = ({ deepEqual, getIn, toJS }) => {
       this.handleFocus = this.handleFocus.bind(this)
       this.handleBlur = this.handleBlur.bind(this)
 
-      this.onChangeFns = Object.keys(props._fields).reduce((acc, name) => ({
-        ...acc, [name]: event => this.handleChange(name, event)
-      }), {})
+      this.onChangeFns = props.names.reduce((acc, name) => {
+        acc[name] = event => this.handleChange(name, event)
+        return acc
+      }, {})
 
-      this.onFocusFns = Object.keys(props._fields).reduce((acc, name) => ({
-        ...acc, [name]: () => this.handleFocus(name)
-      }), {})
+      this.onFocusFns = props.names.reduce((acc, name) => {
+        acc[name] = () => this.handleFocus(name)
+        return acc
+      }, {})
 
-      this.onBlurFns = Object.keys(props._fields).reduce((acc, name) => ({
-        ...acc, [name]: event => this.handleBlur(name, event)
-      }), {})
+      this.onBlurFns = props.names.reduce((acc, name) => {
+        acc[name] = event => this.handleBlur(name, event)
+        return acc
+      }, {})
     }
 
+    componentWillReceiveProps(nextProps) {
+      if (this.props.names !== nextProps.names &&
+        (size(this.props.names) !== size(nextProps.names) ||
+        nextProps.names.some(nextName => !this.props._fields[nextName]))) {
+
+        // names is changed. The cached event handlers need to be updated
+        this.onChangeFns = nextProps.names.reduce((acc, name) => {
+          acc[name] = event => this.handleChange(name, event)
+          return acc
+        }, {})
+
+        this.onFocusFns = nextProps.names.reduce((acc, name) => {
+          acc[name] = () => this.handleFocus(name)
+          return acc
+        }, {})
+
+        this.onBlurFns = nextProps.names.reduce((acc, name) => {
+          acc[name] = event => this.handleBlur(name, event)
+          return acc
+        }, {})
+      }
+    }
+    
     shouldComponentUpdate(nextProps) {
       const nextPropsKeys = Object.keys(nextProps)
       const thisPropsKeys = Object.keys(this.props)

src/Fields.js
@@ -14,12 +14,13 @@ const validateNameProp = prop => {
   }
 }
 
-const createFields = ({ deepEqual, getIn, toJS }) => {
+const createFields = ({ deepEqual, getIn, toJS, size }) => {
 
   const ConnectedFields = createConnectedFields({
     deepEqual,
     getIn,
-    toJS
+    toJS,
+    size
   })
   
   class Fields extends Component {

src/__tests__/Fields.spec.js
@@ -1334,6 +1334,72 @@ const describeFields = (name, structure, combineReducers, expect) => {
       expect(fields.anotherCustomProp).toBe(42)
       expect(fields.customBooleanFlag).toBe(true)
     })
+
+    it('should provide correct prop structure after names change', () => {
+      const store = makeStore()
+      const renderFields = createSpy(() => <div/>).andCallThrough()
+      class Form extends Component {
+        constructor(props) {
+          super(props)
+          this.state = { names: [ 'foo', 'bar', 'deep.dive', 'array[0]' ] }
+          this.changeNames = this.changeNames.bind(this) 
+        }
+        changeNames() {
+          this.setState({ names: [ 'fighter', 'fly.high', 'array[1]' ] })
+        }
+        render() {
+          return (<div>
+            <Fields
+              names={this.state.names}
+              component={renderFields}
+              someCustomProp="testing"
+              anotherCustomProp={42}
+              customBooleanFlag/>
+            <button type="button" onClick={this.changeNames} />
+          </div>)
+        }
+      }
+      const TestForm = reduxForm({ form: 'testForm' })(Form)
+      const dom = TestUtils.renderIntoDocument(
+        <Provider store={store}>
+          <TestForm/>
+        </Provider>
+      )
+
+      const button = TestUtils.findRenderedDOMComponentWithTag(dom, 'button')
+      TestUtils.Simulate.click(button)
+      expect(renderFields).toHaveBeenCalled()
+      expect(renderFields.calls.length).toBe(2)
+      const fields = renderFields.calls[ 1 ].arguments[ 0 ]
+
+      const expectField = field => {
+        expect(field).toExist()
+        expect(field.input).toExist()
+        expect(field.input.onChange).toBeA('function')
+        expect(field.input.onBlur).toBeA('function')
+        expect(field.input.onFocus).toBeA('function')
+        expect(field.meta).toExist()
+        expect(field.meta.pristine).toBe(true)
+        expect(field.meta.dirty).toBe(false)
+        expect(field.someCustomProp).toNotExist()
+        expect(field.anotherCustomProp).toNotExist()
+        expect(field.customBooleanFlag).toNotExist()
+      }
+
+      expectField(fields.fighter)
+      expect(fields.fly).toExist()
+      expectField(fields.fly.high)
+      expect(fields.array).toExist()
+      expectField(fields.array[1])
+      expect(fields.someCustomProp).toBe('testing')
+      expect(fields.anotherCustomProp).toBe(42)
+      expect(fields.customBooleanFlag).toBe(true)
+
+      expect(fields.foo).toNotExist()
+      expect(fields.bar).toNotExist()
+      expect(fields.deep).toNotExist()
+      expect(fields.array[0]).toNotExist()
+    })
   })
 }
 