f3d8eb28247a2ffe237dacebbf8bf87faff16328
Tom Chen <developer@tomchentw.com>
2017-01-27 01:30:53 +0800

    Chore/switch shallowequal to lodash isequalwith (#2506)
    * Add test cases for shallowCompare
    
    * Switch shallowequal to lodash/isEqualWith customizer
    
    * Remove "shallowequal" from dependencies

--- src/util/shallowCompare.js
+++ src/util/shallowCompare.js
@@ -1,9 +1,21 @@
-import shallowEqual from 'shallowequal'
+import { isEqualWith } from 'lodash'
+
+const customizer = (objectValue, otherValue, indexOrkey, object, other, stack) => {
+  // https://lodash.com/docs/4.17.4#isEqualWith
+  if (stack) {
+    // Shallow compares
+    // For 1st level, stack === undefined.
+    //   -> Do nothing (and implicitly return undefined so that it goes to compare 2nd level)
+    // For 2nd level and up, stack !== undefined.
+    //   -> Compare by === operator
+    return objectValue === otherValue
+  }
+}
 
 const shallowCompare = (instance, nextProps, nextState) => {
   return (
-    !shallowEqual(instance.props, nextProps) ||
-    !shallowEqual(instance.state, nextState)
+    !isEqualWith(instance.props, nextProps, customizer) ||
+    !isEqualWith(instance.state, nextState, customizer)
   )
 }
 
