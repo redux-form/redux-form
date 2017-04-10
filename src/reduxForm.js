809c8414121ba2c3de1a5797d259ac4ebb56193e
Martino Pham <mpham@rapid7.com>
2017-04-10 02:27:54 -0700

    fix (possibly stale) reference to validExceptSubmit variable (#2786)
    * instead of referring to a possibly stale reference to validExceptSubmit, refer to this.props.validExceptSubmit
    
    * Changed other usage of validExceptSubmit for clarity

--- src/reduxForm.js
+++ src/reduxForm.js
@@ -410,7 +410,7 @@ const createReduxForm =
           }
 
           submit(submitOrEvent) {
-            const { onSubmit, blur, change, dispatch, validExceptSubmit } = this.props
+            const { onSubmit, blur, change, dispatch } = this.props
 
             if (!submitOrEvent || silenceEvent(submitOrEvent)) {
               // submitOrEvent is an event: fire submit if not already submitting
@@ -426,7 +426,7 @@ const createReduxForm =
                         ...this.props,
                         ...bindActionCreators({ blur, change }, dispatch)
                       },
-                      validExceptSubmit,
+                      this.props.validExceptSubmit,
                       this.asyncValidate,
                       this.getFieldList({ excludeFieldArray: true })
                     )
@@ -444,7 +444,7 @@ const createReduxForm =
                         ...this.props,
                         ...bindActionCreators({ blur, change }, dispatch)
                       },
-                      validExceptSubmit,
+                      this.props.validExceptSubmit,
                       this.asyncValidate,
                       this.getFieldList({ excludeFieldArray: true })
                     )
