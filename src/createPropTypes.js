91da8c17d78d22d527ded286939ec82287bcf4df
Erik Rasmussen <rasmussenerik@gmail.com>
2017-04-19 16:27:15 +0200

    v5.3.6

--- src/createPropTypes.js
+++ src/createPropTypes.js
@@ -1,34 +1,32 @@
 import PropTypes from 'prop-types';
 
-const { any, bool, string, func, object } = PropTypes;
-
 const createPropTypes = () => ({
   // State:
-  active: string,                     // currently active field
-  asyncValidating: bool.isRequired,   // true if async validation is running
-  autofilled: bool,                   // true if set programmatically by autofill
-  dirty: bool.isRequired,             // true if any values are different from initialValues
-  error: any,                         // form-wide error from '_error' key in validation result
-  errors: object,                     // a map of errors corresponding to structure of form data (result of validation)
-  fields: object.isRequired,          // the map of fields
-  formKey: any,                       // the form key if one was provided (used when doing multirecord forms)
-  invalid: bool.isRequired,           // true if there are any validation errors
-  pristine: bool.isRequired,          // true if the values are the same as initialValues
-  submitting: bool.isRequired,        // true if the form is in the process of being submitted
-  submitFailed: bool.isRequired,      // true if the form was submitted and failed for any reason
-  valid: bool.isRequired,             // true if there are no validation errors
-  values: object.isRequired,          // the values of the form as they will be submitted
+  active: PropTypes.string,                     // currently active field
+  asyncValidating: PropTypes.bool.isRequired,   // true if async validation is running
+  autofilled: PropTypes.bool,                   // true if set programmatically by autofill
+  dirty: PropTypes.bool.isRequired,             // true if any values are different from initialValues
+  error: PropTypes.any,                         // form-wide error from '_error' key in validation result
+  errors: PropTypes.object,                     // a map of errors corresponding to structure of form data (result of validation)
+  fields: PropTypes.object.isRequired,          // the map of fields
+  formKey: PropTypes.any,                       // the form key if one was provided (used when doing multirecord forms)
+  invalid: PropTypes.bool.isRequired,           // true if there are any validation errors
+  pristine: PropTypes.bool.isRequired,          // true if the values are the same as initialValues
+  submitting: PropTypes.bool.isRequired,        // true if the form is in the process of being submitted
+  submitFailed: PropTypes.bool.isRequired,      // true if the form was submitted and failed for any reason
+  valid: PropTypes.bool.isRequired,             // true if there are no validation errors
+  values: PropTypes.object.isRequired,          // the values of the form as they will be submitted
 
   // Actions:
-  asyncValidate: func.isRequired,     // function to trigger async validation
-  destroyForm: func.isRequired,       // action to destroy the form's data in Redux
-  handleSubmit: func.isRequired,      // function to submit the form
-  initializeForm: func.isRequired,    // action to initialize form data
-  resetForm: func.isRequired,         // action to reset the form data to previously initialized values
-  touch: func.isRequired,             // action to mark fields as touched
-  touchAll: func.isRequired,          // action to mark ALL fields as touched
-  untouch: func.isRequired,           // action to mark fields as untouched
-  untouchAll: func.isRequired         // action to mark ALL fields as untouched
+  asyncValidate: PropTypes.func.isRequired,     // function to trigger async validation
+  destroyForm: PropTypes.func.isRequired,       // action to destroy the form's data in Redux
+  handleSubmit: PropTypes.func.isRequired,      // function to submit the form
+  initializeForm: PropTypes.func.isRequired,    // action to initialize form data
+  resetForm: PropTypes.func.isRequired,         // action to reset the form data to previously initialized values
+  touch: PropTypes.func.isRequired,             // action to mark fields as touched
+  touchAll: PropTypes.func.isRequired,          // action to mark ALL fields as touched
+  untouch: PropTypes.func.isRequired,           // action to mark fields as untouched
+  untouchAll: PropTypes.func.isRequired         // action to mark ALL fields as untouched
 });
 
 export default createPropTypes;
