1d33c5222d3a6011162c4ab3f2224069fd81bff3
Erik Rasmussen <erik@neon.local>
2017-05-15 16:12:11 +0200

    disabled no-throw-literal eslint rule on a single line

--- examples/material-ui/src/asyncValidate.js
+++ examples/material-ui/src/asyncValidate.js
@@ -4,7 +4,8 @@ const asyncValidate = (values /*, dispatch */) => {
   return sleep(1000).then(() => {
     // simulate server latency
     if (['foo@foo.com', 'bar@bar.com'].includes(values.email)) {
-      throw {email: 'Email already Exists'}
+      // eslint-disable-next-line no-throw-literal
+      throw { email: 'Email already Exists' }
     }
   })
 }
