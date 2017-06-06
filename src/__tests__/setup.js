257c21a16c25f82d01c0a8832b70358d3187f7a9
Erik Rasmussen <erik@neon.local>
2017-06-06 17:09:33 +0200

    changed jsdom syntax...?

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,4 +1,6 @@
-import {JSDOM} from 'jsdom'
+import jsdom from 'jsdom'
+
+const {JSDOM} = jsdom
 
 global.window = new JSDOM('<!doctype html><html><body></body></html>').window
 global.document = global.window.document
