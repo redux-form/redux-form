33eb537620affccc86f5a4b315c261942f33e426
Adam Misiorny <adam.misiorny@gmail.com>
2017-06-06 12:39:21 +0200

    add support for plain objects in keys extractor from immutable structure

--- src/structure/immutable/keys.js
+++ src/structure/immutable/keys.js
@@ -1,7 +1,14 @@
 import {Iterable, List} from 'immutable'
+import plainKeys from '../plain/keys'
 
 const empty = List()
 
-const keys = value => (Iterable.isIterable(value) ? value.keySeq() : empty)
+const keys = value => {
+  if (Iterable.isIterable(value)) {
+    return value.keySeq()
+  }
+
+  return value ? List(plainKeys(value)) : empty
+}
 
 export default keys
