84b9f41ab5170fad0665eff99e628d966fa41666
ncphillips <ncphillips.19@gmail.com>
2017-06-08 15:36:59 -0300

    keys now works with arrays

--- src/structure/immutable/keys.js
+++ src/structure/immutable/keys.js
@@ -4,10 +4,14 @@ import plainKeys from '../plain/keys'
 const empty = List()
 
 const keys = value => {
+  if (List.isList(value)) {
+    return value.map(i => i.name)
+  }
+  
   if (Iterable.isIterable(value)) {
     return value.keySeq()
   }
-
+  
   return value ? List(plainKeys(value)) : empty
 }
 
