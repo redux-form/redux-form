1e367943abfa98aa1f134fccc82cdeb7309c95e8
Gustavo Henke <guhenke@gmail.com>
2017-06-18 19:50:55 +1000

    Add anchors to each heading (#3076)
    Closes #2411

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import marked from 'marked'
 import Prism from '../vendor/prism'
+import styles from './Markdown.scss'
 
 const prettify = markdown =>
   markdown.replace(/```(?:javascript|js)([\s\S]+?)```/g,
     (match, code) =>
       `<pre class="language-jsx"><code class="language-jsx">${Prism.highlight(code, Prism.languages.jsx)}</code></pre>`)
 
+const renderer = new marked.Renderer()
+renderer.heading = (text, level) => {
+  const id = text.toLowerCase().replace(/[^\w]+/g, '-')
+  return `<h${level} class="${styles.heading}" id="${id}">${text} <a href="#${id}">#</a></h${level}>`
+}
+
 const Markdown = ({ content }) => {
-  return <div dangerouslySetInnerHTML={{ __html: marked(prettify(content)) }}/>
+  return <div dangerouslySetInnerHTML={{ __html: marked(prettify(content), { renderer }) }}/>
 }
 
 export default Markdown
