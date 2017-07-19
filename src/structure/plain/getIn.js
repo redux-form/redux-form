562c0b0b379d37ecef81f04d90079c467952a81a
Jon R <jmrapp1270@gmail.com>
2017-07-19 09:51:40 -0400

    Added support for immutable states to be used with the getIn function

--- src/structure/plain/getIn.js
+++ src/structure/plain/getIn.js
@@ -1,5 +1,6 @@
 // @flow
 import { toPath } from 'lodash'
+import Immutable from 'immutable';
 
 const getIn = (state: Object | Array<*>, field: string): any => {
   if (!state) {
@@ -12,9 +13,13 @@ const getIn = (state: Object | Array<*>, field: string): any => {
     return undefined
   }
 
+  if (Immutable.Iterable.isIterable(state)) {
+    state = state.toJSON();
+  }
+
   let result: any = state
   for (let i = 0; i < length && result; ++i) {
-    result = result[path[i]]
+    result = result[path[i]];
   }
 
   return result
