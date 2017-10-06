e10d92bc5b1811d95b7d2d71ed1f7a92f516d780
delijah <raffael.wannenmacher@gmail.com>
2017-10-06 11:47:49 +0200

    Use deep equal to compare radio input values (#3430)
    * Using deep equal to detect changes on radio input values
    
    * Fixed tests for createFieldProps
    
    * Implemented new tests

--- src/__tests__/createFieldProps.spec.js
+++ src/__tests__/createFieldProps.spec.js
@@ -7,7 +7,7 @@ import immutableExpectations from '../structure/immutable/__tests__/expectations
 import tmp from 'tmp'
 
 const describeCreateFieldProps = (name, structure, setup) => {
-  const { empty, getIn, fromJS, toJS } = structure
+  const { empty, getIn, fromJS, toJS, deepEqual } = structure
 
   describe(name, () => {
     beforeAll(() => {
@@ -16,31 +16,31 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should pass value through', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', { value: 'hello' }).input.value
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', { value: 'hello' }).input.value
       ).toBe('hello')
     })
 
     it('should pass dirty/pristine through', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           dirty: false,
           pristine: true
         }).meta.dirty
       ).toBe(false)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           dirty: false,
           pristine: true
         }).meta.pristine
       ).toBe(true)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           dirty: true,
           pristine: false
         }).meta.dirty
       ).toBe(true)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           dirty: true,
           pristine: false
         }).meta.pristine
@@ -49,7 +49,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should pass initial value through', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', { initial: 'hello' }).meta
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', { initial: 'hello' }).meta
           .initial
       ).toBe('hello')
     })
@@ -57,7 +57,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
     it('should provide onBlur', () => {
       const onBlur = jest.fn()
       expect(onBlur).not.toHaveBeenCalled()
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         onBlur
       })
@@ -70,7 +70,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
     it('should provide onChange', () => {
       const onChange = jest.fn()
       expect(onChange).not.toHaveBeenCalled()
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         onChange
       })
@@ -83,7 +83,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
     it('should provide onFocus', () => {
       const onFocus = jest.fn()
       expect(onFocus).not.toHaveBeenCalled()
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         onFocus
       })
@@ -95,7 +95,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should provide onDragStart', () => {
       const onDragStart = jest.fn()
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         onDragStart
       })
@@ -104,7 +104,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should provide onDrop', () => {
       const onDrop = jest.fn()
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         onDrop
       })
@@ -112,12 +112,12 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read active from state', () => {
-      const inactiveResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const inactiveResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(inactiveResult.meta.active).toBe(false)
-      const activeResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const activeResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: fromJS({
           active: true
@@ -127,11 +127,11 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should pass along submitting flag', () => {
-      const notSubmittingResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const notSubmittingResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar'
       })
       expect(notSubmittingResult.meta.submitting).toBe(false)
-      const submittingResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const submittingResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         submitting: true
       })
@@ -139,11 +139,11 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should pass along submitFailed flag', () => {
-      const notFailedResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const notFailedResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar'
       })
       expect(notFailedResult.meta.submitFailed).toBe(false)
-      const failedResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const failedResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         submitFailed: true
       })
@@ -151,11 +151,11 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should pass along all custom state props', () => {
-      const pristineResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const pristineResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar'
       })
       expect(pristineResult.meta.customProp).toBe(undefined)
-      const customResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const customResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: fromJS({
           customProp: 'my-custom-prop'
@@ -165,11 +165,11 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should not override canonical props with custom props', () => {
-      const pristineResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const pristineResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar'
       })
       expect(pristineResult.meta.customProp).toBe(undefined)
-      const customResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const customResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         submitting: true,
         state: fromJS({
@@ -180,12 +180,12 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read touched from state', () => {
-      const untouchedResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const untouchedResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(untouchedResult.meta.touched).toBe(false)
-      const touchedResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const touchedResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: fromJS({
           touched: true
@@ -195,12 +195,12 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read visited from state', () => {
-      const notVisitedResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const notVisitedResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(notVisitedResult.meta.visited).toBe(false)
-      const visitedResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const visitedResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: fromJS({
           visited: true
@@ -210,14 +210,14 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read sync errors from prop', () => {
-      const noErrorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const noErrorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(noErrorResult.meta.error).toBeFalsy()
       expect(noErrorResult.meta.valid).toBe(true)
       expect(noErrorResult.meta.invalid).toBe(false)
-      const errorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const errorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty,
         syncError: 'This is an error'
@@ -228,12 +228,12 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read sync warnings from prop', () => {
-      const noWarningResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const noWarningResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(noWarningResult.meta.warning).toBeFalsy()
-      const warningResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const warningResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty,
         syncWarning: 'This is an warning'
@@ -242,14 +242,14 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read async errors from state', () => {
-      const noErrorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const noErrorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(noErrorResult.meta.error).toBeFalsy()
       expect(noErrorResult.meta.valid).toBe(true)
       expect(noErrorResult.meta.invalid).toBe(false)
-      const errorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const errorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty,
         syncError: 'This is an error'
@@ -260,14 +260,14 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should read submit errors from state', () => {
-      const noErrorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const noErrorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(noErrorResult.meta.error).toBeFalsy()
       expect(noErrorResult.meta.valid).toBe(true)
       expect(noErrorResult.meta.invalid).toBe(false)
-      const errorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const errorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty,
         submitError: 'This is an error'
@@ -278,14 +278,14 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should prioritize sync errors over async or submit errors', () => {
-      const noErrorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const noErrorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty
       })
       expect(noErrorResult.meta.error).toBeFalsy()
       expect(noErrorResult.meta.valid).toBe(true)
       expect(noErrorResult.meta.invalid).toBe(false)
-      const errorResult = createFieldProps({ getIn, toJS }, 'foo', {
+      const errorResult = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         asyncError: 'async error',
         submitError: 'submit error',
@@ -297,7 +297,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should pass through other props', () => {
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty,
         someOtherProp: 'dog',
@@ -310,7 +310,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should pass through other props using props prop', () => {
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         value: 'bar',
         state: empty,
         props: {
@@ -326,20 +326,20 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should set checked for checkboxes', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           state: empty,
           type: 'checkbox'
         }).input.checked
       ).toBe(false)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           value: true,
           state: empty,
           type: 'checkbox'
         }).input.checked
       ).toBe(true)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           value: false,
           state: empty,
           type: 'checkbox'
@@ -349,14 +349,14 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should set checked for radio buttons', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           state: empty,
           type: 'radio',
           _value: 'bar'
         }).input.checked
       ).toBe(false)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           value: 'bar',
           state: empty,
           type: 'radio',
@@ -364,18 +364,41 @@ const describeCreateFieldProps = (name, structure, setup) => {
         }).input.checked
       ).toBe(true)
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           value: 'baz',
           state: empty,
           type: 'radio',
           _value: 'bar'
         }).input.checked
       ).toBe(false)
+      expect(
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
+          state: empty,
+          type: 'radio',
+          _value: { randomName: 'bar' }
+        }).input.checked
+      ).toBe(false)
+      expect(
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
+          value: { randomName: 'bar' },
+          state: empty,
+          type: 'radio',
+          _value: { randomName: 'bar' }
+        }).input.checked
+      ).toBe(true)
+      expect(
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
+          value: { randomName: 'baz' },
+          state: empty,
+          type: 'radio',
+          _value: { randomName: 'bar' }
+        }).input.checked
+      ).toBe(false)
     })
 
     it('should default value to [] for multi-selects', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           state: empty,
           type: 'select-multiple'
         }).input.value
@@ -384,7 +407,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
 
     it('should default value to undefined for file inputs', () => {
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           state: empty,
           type: 'file'
         }).input.value
@@ -394,7 +417,7 @@ const describeCreateFieldProps = (name, structure, setup) => {
     it('should update value to selected file for file inputs', () => {
       let tmpFile = tmp.fileSync()
       expect(
-        createFieldProps({ getIn, toJS }, 'foo', {
+        createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
           value: [tmpFile],
           state: empty,
           type: 'file'
@@ -403,14 +426,14 @@ const describeCreateFieldProps = (name, structure, setup) => {
     })
 
     it('should replace undefined value with empty string', () => {
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         state: empty
       })
       expect(result.input.value).toBe('')
     })
 
     it('should not format value when format prop is null', () => {
-      const result = createFieldProps({ getIn, toJS }, 'foo', {
+      const result = createFieldProps({ getIn, toJS, deepEqual }, 'foo', {
         state: empty,
         value: null,
         format: null
