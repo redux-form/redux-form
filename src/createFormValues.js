701fdc0d540ef905b3d9b005c1a4c65ef967e954
Manuel Baclet <manuel.baclet@thetribe.io>
2018-02-01 11:45:57 +0100

    Fix for formValues not reacting to props change (#3794)

--- src/createFormValues.js
+++ src/createFormValues.js
@@ -1,11 +1,12 @@
 // @flow
+import { isEmpty, isEqual, mapValues } from 'lodash'
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import prefixName from './util/prefixName'
 import type { ComponentType } from 'react'
 import type { Structure, ReactContext } from './types'
-import type { FormValuesInterface, PropPath } from './formValues.types'
+import type { FormValuesInterface } from './formValues.types'
 
 const createValues = ({ getIn }: Structure<*, *>): FormValuesInterface => (
   firstArg: string | Object | Function,
@@ -17,6 +18,7 @@ const createValues = ({ getIn }: Structure<*, *>): FormValuesInterface => (
     class FormValues extends React.Component<Object> {
       context: ReactContext
       Component: ComponentType<*>
+      _valuesMap: Object
 
       constructor(props: Object, context: ReactContext) {
         super(props, context)
@@ -25,46 +27,12 @@ const createValues = ({ getIn }: Structure<*, *>): FormValuesInterface => (
             'formValues() must be used inside a React tree decorated with reduxForm()'
           )
         }
-        let valuesMap: PropPath[]
-        const resolvedFirstArg: string | Object =
-          typeof firstArg === 'function' ? firstArg(props) : firstArg
-        if (typeof resolvedFirstArg === 'string') {
-          valuesMap = [
-            resolvedFirstArg,
-            ...rest
-          ].map((k: string): PropPath => ({
-            prop: k,
-            path: k
-          }))
-        } else {
-          const config: Object = resolvedFirstArg
-          valuesMap = Object.keys(config).map(k => ({
-            prop: k,
-            path: config[k]
-          }))
+        this.updateComponent(props)
+      }
+      componentWillReceiveProps(props) {
+        if (typeof firstArg === 'function') {
+          this.updateComponent(props)
         }
-        if (!valuesMap.length) {
-          throw new Error(
-            'formValues(): You must specify values to get as formValues(name1, name2, ...) or formValues({propName1: propPath1, ...}) or formValues((props) => name) or formValues((props) => ({propName1: propPath1, ...}))'
-          )
-        }
-        const formValuesSelector = (_, { sectionPrefix }) => {
-          // Yes, we're only using connect() for listening to updates.
-          // The second argument needs to be there so that connect calls
-          // the selector when props change
-          const { getValues } = this.context._reduxForm
-          const props = {}
-          const values = getValues()
-          valuesMap.forEach(
-            ({ prop, path }) =>
-              (props[prop] = getIn(values, prefixName(this.context, path)))
-          )
-          return props
-        }
-        this.Component = connect(
-          formValuesSelector,
-          () => ({}) // ignore dispatch
-        )(({ sectionPrefix, ...otherProps }) => <Component {...otherProps} />)
       }
       render() {
         const { Component } = this
@@ -75,6 +43,51 @@ const createValues = ({ getIn }: Structure<*, *>): FormValuesInterface => (
             {...this.props}
           />
         )
+      }
+      updateComponent(props) {
+        let valuesMap: Object
+        const resolvedFirstArg: string | Object =
+          typeof firstArg === 'function' ? firstArg(props) : firstArg
+        if (typeof resolvedFirstArg === 'string') {
+          valuesMap = rest.reduce(
+            (result, k) => {
+              result[k] = k
+              return result
+            },
+            { [resolvedFirstArg]: resolvedFirstArg }
+          )
+        } else {
+          valuesMap = resolvedFirstArg
+        }
+        if (isEmpty(valuesMap)) {
+          // maybe that empty valuesMap is ok if firstArg is a function?
+          // if this is the case, we probably should set this.Component = Component
+          throw new Error(
+            'formValues(): You must specify values to get as formValues(name1, name2, ...) or formValues({propName1: propPath1, ...}) or formValues((props) => name) or formValues((props) => ({propName1: propPath1, ...}))'
+          )
+        }
+        if (isEqual(valuesMap, this._valuesMap)) {
+          // no change in valuesMap
+          return
+        }
+        this._valuesMap = valuesMap
+        this.setComponent()
+      }
+      setComponent() {
+        const formValuesSelector = (_, { sectionPrefix }) => {
+          // Yes, we're only using connect() for listening to updates.
+          // The second argument needs to be there so that connect calls
+          // the selector when props change
+          const { getValues } = this.context._reduxForm
+          const values = getValues()
+          return mapValues(this._valuesMap, (path: string) =>
+            getIn(values, prefixName(this.context, path))
+          )
+        }
+        this.Component = connect(
+          formValuesSelector,
+          () => ({}) // ignore dispatch
+        )(({ sectionPrefix, ...otherProps }) => <Component {...otherProps} />)
       }
     }
     FormValues.contextTypes = {
