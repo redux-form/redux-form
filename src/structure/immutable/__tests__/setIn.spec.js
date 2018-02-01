redux-form/redux-form

commit f327c2105b8b3a0ebd6ad8e6b54df8f1226403fb

fix erikras/redux-form#3489 (#3692)

src/structure/immutable/setIn.js
@@ -10,7 +10,7 @@ const undefinedArrayMerge = (previous, next) =>
 
 const mergeLists = (original, value) =>
   original && List.isList(original)
-    ? original.mergeDeepWith(undefinedArrayMerge, value)
+    ? original.toMap().mergeDeepWith(undefinedArrayMerge, value.toMap()).toList()
     : value
 
 /*

commit bfac4616a8884db3fd30857426c72c261fd837c4

Fixes issue introduced in #3692 where arrays longer than 32 elements would yield unexpected behavior

src/structure/immutable/setIn.js
@@ -1,5 +1,6 @@
 // @flow
-import { List, Map } from 'immutable'
+// $FlowFixMe: Need to ignore the mergeDeepWith import until we are on Immutable@4.0.0
+import { List, Map, mergeDeepWith } from 'immutable'
 import { toPath } from 'lodash'
 import type { Map as ImmutableMap, List as ImmutableList } from 'immutable'
 
@@ -8,10 +9,19 @@ const arrayPattern = /\[(\d+)\]/
 const undefinedArrayMerge = (previous, next) =>
   next !== undefined ? next : previous
 
-const mergeLists = (original, value) =>
-  original && List.isList(original)
-    ? original.toMap().mergeDeepWith(undefinedArrayMerge, value.toMap()).toList()
-    : value
+const mergeLists = (original, value) => {
+  if (original && List.isList(original)) {
+    if (original.mergeDeepWith) {
+      // In this case we are using Immutable.js@3.x since we still have List.mergeDeepWith
+      return original.mergeDeepWith(undefinedArrayMerge, value)
+    }
+
+    // Immutable 4.x users will end here.
+    return mergeDeepWith(undefinedArrayMerge, original, value)
+  }
+
+  return value
+}
 
 /*
  * ImmutableJS' setIn function doesn't support array (List) creation

src/structure/immutable/__tests__/setIn.spec.js
@@ -312,4 +312,25 @@ describe('structure.immutable.setIn', () => {
     let b0c = b0.get('c')
     expect(b0c).toEqual('123')
   })
+  it('should handle arrays with length 64', () => {
+    let initial = fromJS({
+      a: {
+        b: []
+      }
+    })
+
+    let result = setIn(initial, 'a.b[64].d', '12')
+
+    let a = result.get('a')
+    expect(a).toBeTruthy()
+
+    let b = a.get('b')
+    expect(b).toBeTruthy()
+
+    let b64 = b.get(64)
+    expect(b64).toBeTruthy()
+
+    let b64d = b64.get('d')
+    expect(b64d).toEqual('12')
+  })
 })