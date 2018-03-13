24b8ca950a938ac5aa885deb89175ea3ad2dbe91
Viljar Kärgenberg <viljar.kargenberg@iglu.ee>
2018-03-13 14:30:31 +0200

    Field not validating after re-mount (#3566)

--- src/createReduxForm.js
+++ src/createReduxForm.js
@@ -619,6 +619,9 @@ const createReduxForm = (structure: Structure<*, *>) => {
               if (!this.fieldCounts[name]) {
                 delete this.fieldValidators[name]
                 delete this.fieldWarners[name]
+                this.lastFieldValidatorKeys = this.lastFieldValidatorKeys.filter(
+                  item => item !== name
+                )
               }
             } else {
               unregisterField(name, false)
