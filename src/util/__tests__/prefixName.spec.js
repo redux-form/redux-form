742225d0ba8713750835cbb564578c791ee1533a
Benjamin Michel <benjamin.michel@habx.fr>
2018-07-05 16:16:42 +0200

    Added tests for prefixName and handle name = 0 and name = undefined

--- src/util/__tests__/prefixName.spec.js
+++ src/util/__tests__/prefixName.spec.js
@@ -18,4 +18,31 @@ describe('prefixName', () => {
     }
     expect(prefixName(context, 'bar')).toBe('bar')
   })
+
+  it('should ignore empty name', () => {
+    const context = {
+      _reduxForm: {
+        sectionPrefix: 'foo'
+      }
+    }
+    expect(prefixName(context, '')).toBe('foo')
+  })
+
+  it('should handle name = 0', () => {
+    const context = {
+      _reduxForm: {
+        sectionPrefix: 'foo'
+      }
+    }
+    expect(prefixName(context, 0)).toBe('foo.0')
+  })
+
+  it('should handle name = undefined', () => {
+    const context = {
+      _reduxForm: {
+        sectionPrefix: 'foo'
+      }
+    }
+    expect(prefixName(context)).toBe('foo')
+  })
 })
