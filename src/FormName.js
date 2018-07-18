eb7c6fbfff545c994ef7425407698ac66d83a3c3
Andy Edwards <jedwards@fastmail.com>
2018-07-18 13:09:35 -0500

    feat(FormName): also pass sectionPrefix (#4119)

--- src/FormName.js
+++ src/FormName.js
@@ -5,13 +5,17 @@ import PropTypes from 'prop-types'
 import type {ReactContext} from './types'
 
 export type Props = {
-  +children: (props: {form: string}) => React.Node,
+  +children: (props: {form: string, sectionPrefix: ?string}) => React.Node,
 }
 
-const FormName = ({children}: Props, {_reduxForm}: ReactContext): React.Node => children({form: _reduxForm && _reduxForm.form})
+const FormName = ({children}: Props, {_reduxForm}: ReactContext): React.Node => children({
+  form: _reduxForm && _reduxForm.form,
+  sectionPrefix: _reduxForm && _reduxForm.sectionPrefix,
+})
 FormName.contextTypes = {
   _reduxForm: PropTypes.shape({
     form: PropTypes.string.isRequired,
+    sectionPrefix: PropTypes.string,
   }).isRequired,
 }
 
