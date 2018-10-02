383df8d191d3cc691eafbdab6326ebbd75235d8d
Renato Augusto Gama dos Santos <renato_0603@hotmail.com>
2018-10-02 07:13:36 -0300

    Remove * imports to avoid warning in React 15 (#4140)
    * fix: Remove * imports to avoid warning in React 15
    
    * fix: remove unused React from createReduxForm
    
    * fix: typo in FieldArrayProps

--- src/ConnectedFields.js
+++ src/ConnectedFields.js
@@ -1,5 +1,5 @@
 // @flow
-import * as React from 'react'
+import { Component, createElement } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import createFieldProps from './createFieldProps'
@@ -29,11 +29,11 @@ const createConnectedFields = (structure: Structure<*, *>) => {
     return warning && warning._warning ? warning._warning : warning
   }
 
-  class ConnectedFields extends React.Component<Props> {
+  class ConnectedFields extends Component<Props> {
     onChangeFns = {}
     onFocusFns = {}
     onBlurFns = {}
-    ref: ?React.Component<*>
+    ref: ?Component<*>
 
     constructor(props: Props) {
       super(props)
@@ -124,7 +124,7 @@ const createConnectedFields = (structure: Structure<*, *>) => {
       }
     }
 
-    saveRef = (ref: ?React.Component<*>) => {
+    saveRef = (ref: ?Component<*>) => {
       this.ref = ref
     }
 
@@ -154,7 +154,7 @@ const createConnectedFields = (structure: Structure<*, *>) => {
         props.ref = this.saveRef
       }
 
-      return React.createElement(component, { ...props, ...custom })
+      return createElement(component, { ...props, ...custom })
     }
   }
 
--- src/FormName.js
+++ src/FormName.js
@@ -1,22 +1,23 @@
 // @flow
 
-import * as React from 'react'
 import PropTypes from 'prop-types'
-import type {ReactContext} from './types'
+import type { Node } from 'react'
+import type { ReactContext } from './types'
 
 export type Props = {
-  +children: (props: {form: string, sectionPrefix: ?string}) => React.Node,
+  +children: (props: { form: string, sectionPrefix: ?string }) => Node
 }
 
-const FormName = ({children}: Props, {_reduxForm}: ReactContext): React.Node => children({
-  form: _reduxForm && _reduxForm.form,
-  sectionPrefix: _reduxForm && _reduxForm.sectionPrefix,
-})
+const FormName = ({ children }: Props, { _reduxForm }: ReactContext): Node =>
+  children({
+    form: _reduxForm && _reduxForm.form,
+    sectionPrefix: _reduxForm && _reduxForm.sectionPrefix
+  })
 FormName.contextTypes = {
   _reduxForm: PropTypes.shape({
     form: PropTypes.string.isRequired,
-    sectionPrefix: PropTypes.string,
-  }).isRequired,
+    sectionPrefix: PropTypes.string
+  }).isRequired
 }
 
 export default FormName
