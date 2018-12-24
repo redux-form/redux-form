982cb423d4d6abe295b33dbe6201120c8f566ff0
vtaits <taizvadim@gmail.com>
2018-12-24 19:42:21 +0300

    Add @babel/plugin-transform-runtime (#4324) (#4329)

--- .babelrc.js
+++ .babelrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ],
   plugins: [
     'lodash',
+    '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-flow-strip-types',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
