34b6bdcd4d6fec2320ac73c9414b9b239a2f6ed6
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2019-03-22 11:40:12 +0100

    fix: better detection of when mapToProps funcs depend on props

--- src/wrapMapStateToProps.js
+++ src/wrapMapStateToProps.js
@@ -1,9 +1,11 @@
+import {getDependsOnOwnProps} from './helpers';
+
 const wrapMapStateToProps = (mapStateToProps, getForm) => {
   if (mapStateToProps) {
     if (typeof mapStateToProps !== 'function') {
       throw new Error('mapStateToProps must be a function');
     }
-    if (mapStateToProps.length > 1) {
+    if (getDependsOnOwnProps(mapStateToProps)) {
       return (state, ownProps) => ({
         ...mapStateToProps(state, ownProps),
         form: getForm(state)
