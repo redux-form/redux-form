2d433630b8b570f3fa97afe3e72445c81e0dd06d
Timofey Kachalov <sanex3339@users.noreply.github.com>
2019-03-28 18:37:06 +0300

    Fixed plain/deepEqual to correct update values for NaN -> 0 value change

--- src/structure/plain/deepEqual.js
+++ src/structure/plain/deepEqual.js
@@ -6,8 +6,8 @@ const customizer = (obj: any, other: any) => {
   if (obj === other) return true
 
   if (!obj && !other) {
-    const objIsEmpty = obj === null || obj === undefined || obj === ''
-    const otherIsEmpty = other === null || other === undefined || other === ''
+    const objIsEmpty = obj === null || obj === undefined || obj === ''|| isNaN(obj)
+    const otherIsEmpty = other === null || other === undefined || other === '' || isNaN(other);
     return objIsEmpty === otherIsEmpty
   }
 
