4f0ca0db5670186d5e54d56076c9924b43483a88
Timofey Kachalov <sanex3339@users.noreply.github.com>
2019-03-28 20:29:43 +0300

    Plain/deepEqual conditions refactoring

--- src/structure/plain/deepEqual.js
+++ src/structure/plain/deepEqual.js
@@ -2,13 +2,17 @@
 import React from 'react'
 import { isEqualWith } from 'lodash'
 
+const isEmpty = (obj: any) => {
+    const isNil = obj === null || obj === undefined
+    
+    return isNil || obj === '' || isNaN(obj)
+}
+
 const customizer = (obj: any, other: any) => {
   if (obj === other) return true
 
   if (!obj && !other) {
-    const objIsEmpty = obj === null || obj === undefined || obj === ''|| isNaN(obj)
-    const otherIsEmpty = other === null || other === undefined || other === '' || isNaN(other)
-    return objIsEmpty === otherIsEmpty
+    return isEmpty(obj) === isEmpty(other)
   }
 
   if (obj && other && obj._error !== other._error) return false
