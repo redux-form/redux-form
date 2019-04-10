0ababab8d3dd2e76b9905aa5a26dd8aefe9a0562
Timofey Kachalov <sanex3339@users.noreply.github.com>
2019-04-10 10:28:16 +0300

    Fixed deepEqual NaN -> 0 comparison  (#4416)
    * Fixed plain/deepEqual to correct update values for NaN -> 0 value change
    
    * Fixed immutable/deepEqual to correct update values for NaN -> 0 value change
    
    * Added tests for plain/deepEqual NaN, 0 comparison
    
    * Added test for immutable/deepEqual NaN, 0 comparison
    
    * plain/deepEqual removed semicolon
    
    * Immutable/deepEqual condition refactoring
    
    * Plain/deepEqual conditions refactoring
    
    * deepEqual isEmpty check: use of lodash/isNil helper

--- src/structure/immutable/deepEqual.js
+++ src/structure/immutable/deepEqual.js
@@ -1,14 +1,16 @@
 // @flow
 import { Iterable } from 'immutable'
 
-import { isEqualWith } from 'lodash'
+import { isEqualWith, isNil } from 'lodash'
+
+const isEmpty = (obj: any) => {
+  return isNil(obj) || obj === '' || isNaN(obj)
+}
 
 const customizer = (obj: any, other: any) => {
   if (obj === other) return true
   if (!obj && !other) {
-    const objIsEmpty = obj === null || obj === undefined || obj === ''
-    const otherIsEmpty = other === null || other === undefined || other === ''
-    return objIsEmpty === otherIsEmpty
+    return isEmpty(obj) === isEmpty(other)
   }
 
   if (Iterable.isIterable(obj) && Iterable.isIterable(other)) {
--- src/structure/plain/deepEqual.js
+++ src/structure/plain/deepEqual.js
@@ -1,14 +1,16 @@
 // @flow
 import React from 'react'
-import { isEqualWith } from 'lodash'
+import { isEqualWith, isNil } from 'lodash'
+
+const isEmpty = (obj: any) => {
+  return isNil(obj) || obj === '' || isNaN(obj)
+}
 
 const customizer = (obj: any, other: any) => {
   if (obj === other) return true
 
   if (!obj && !other) {
-    const objIsEmpty = obj === null || obj === undefined || obj === ''
-    const otherIsEmpty = other === null || other === undefined || other === ''
-    return objIsEmpty === otherIsEmpty
+    return isEmpty(obj) === isEmpty(other)
   }
 
   if (obj && other && obj._error !== other._error) return false
