986ea619b52603de43309010078d67a243a5de69
Thomas Rich <tnrich@teselagen.com>
2019-07-29 08:16:32 -0700

    Update src/createReducer.js
    Co-Authored-By: Andrew Luca <thendrluca@gmail.com>

--- src/createReducer.js
+++ src/createReducer.js
@@ -303,7 +303,7 @@ function createReducer<M, L>(structure: Structure<M, L>) {
     ) {
       let result = state
       const initial = getIn(result, `initial.${field}`)
-      if (payload === '') {
+      if ((initial === undefined && payload === '') || payload === undefined) {
         result = deleteInWithCleanUp(result, `values.${field}`)
       } else if (isFunction(payload)) {
         const fieldCurrentValue = getIn(state, `values.${field}`)
