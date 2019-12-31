8192a818f8a01f7071451c97f04d163450d8c7c8
Andrew Luca <thendrluca@gmail.com>
2019-12-31 12:05:13 +0200

    chore: move examples to a new repository
    Examples were moved to https://github.com/redux-form/redux-form-examples

--- examples/immutable/src/reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux-immutablejs'
-import { reducer as form } from 'redux-form/immutable' // <--- immutable import
-
-const reducer = combineReducers({ form })
-
-export default reducer
--- examples/wizard/src/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { reducer as reduxFormReducer } from 'redux-form'
-
-const reducer = combineReducers({
-  form: reduxFormReducer
-})
-
-export default reducer
