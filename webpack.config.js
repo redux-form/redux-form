08f0fa3b22bb00a63bbdfffc8b9dffb0c5a69d57
Andrew Luca <thendrluca@gmail.com>
2020-04-13 02:25:38 +0300

    chore: remove unused `lodash-es`

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,34 @@ const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
 const env = process.env.NODE_ENV
 
-const reactExternal = {
-  root: 'React',
-  commonjs2: 'react',
-  commonjs: 'react',
-  amd: 'react'
-}
-
-const reduxExternal = {
-  root: 'Redux',
-  commonjs2: 'redux',
-  commonjs: 'redux',
-  amd: 'redux'
-}
-
-const reactReduxExternal = {
-  root: 'ReactRedux',
-  commonjs2: 'react-redux',
-  commonjs: 'react-redux',
-  amd: 'react-redux'
-}
-
-const immutableExternal = {
-  root: 'Immutable',
-  commonjs2: 'immutable',
-  commonjs: 'immutable',
-  amd: 'immutable'
-}
-
 module.exports = {
-  devtool: env === 'production' ? 'source-map' : false,
   externals: {
-    react: reactExternal,
-    redux: reduxExternal,
-    'react-redux': reactReduxExternal,
-    immutable: immutableExternal
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    redux: {
+      root: 'Redux',
+      commonjs2: 'redux',
+      commonjs: 'redux',
+      amd: 'redux'
+    },
+    'react-redux': {
+      root: 'ReactRedux',
+      commonjs2: 'react-redux',
+      commonjs: 'react-redux',
+      amd: 'react-redux'
+    },
+    immutable: {
+      root: 'Immutable',
+      commonjs2: 'immutable',
+      commonjs: 'immutable',
+      amd: 'immutable'
+    }
   },
+  devtool: env === 'production' ? 'source-map' : false,
   mode: env === 'production' ? 'production' : 'development',
   module: {
     rules: [
