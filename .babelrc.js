1b89f1f637016b7c7a2f009b9f2df0549d092896
Andrew Luca <thendrluca@gmail.com>
2020-04-13 01:18:41 +0300

    chore: remove unused babel plugins and presets
    **Added**
    
    This was added instead of `@babel/preset-flow` because was used explicit
    in babel config.
    `@babel/preset-flow` didn't have any value
    
    ```
    @babel/plugin-transform-flow-strip-types
    ```
    
    **Included in
    [`@babel/preset-env`](https://github.com/babel/babel/blob/cab1e79ba706249876a41f88d288e7c1c0bea673/packages/babel-preset-env/package.json)**
    
    ```
    @babel/plugin-proposal-json-strings
    @babel/plugin-proposal-numeric-separator
    @babel/plugin-syntax-dynamic-import
    @babel/plugin-transform-modules-commonjs
    ```
    
    **Included in
    [`@babel/preset-react`](https://github.com/babel/babel/blob/d3cf5fb5f4b46a1f7e9e33f2d24a466e3ea1e50f/packages/babel-preset-react/package.json)**
    
    ```
    @babel/plugin-transform-react-jsx-source
    ```
    
    **Included in `@babel/plugin-transform-runtime` (manual tested)**
    
    ```
    @babel/plugin-transform-regenerator
    babel-plugin-syntax-async-functions
    ```
    
    **Banned by flow-type, or unused (some under flag)**
    
    ```
    @babel/plugin-proposal-decorators
    @babel/plugin-proposal-export-namespace-from
    @babel/plugin-proposal-throw-expressions
    ```
    
    **Unused plugins**
    
    ```
    @babel/plugin-proposal-function-sent
    @babel/plugin-syntax-import-meta
    @babel/register
    babel-preset-es2015-no-commonjs
    ```

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,4 @@
-const { NODE_ENV, BABEL_ENV } = process.env
-const test = NODE_ENV === 'test'
-const isEs = BABEL_ENV === 'es'
-const loose = true
+const isEs = process.env.BABEL_ENV === 'es'
 
 module.exports = {
   presets: [
@@ -9,30 +6,15 @@ module.exports = {
       '@babel/preset-env',
       {
         modules: isEs ? false : 'commonjs',
-        loose
+        loose: true
       }
     ],
-    '@babel/preset-react',
-    '@babel/preset-flow'
+    '@babel/preset-react'
   ],
   plugins: [
-    'lodash',
+    'babel-plugin-lodash',
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    ['@babel/plugin-proposal-class-properties', { loose }],
-    '@babel/plugin-proposal-json-strings',
-    [
-      '@babel/plugin-proposal-decorators',
-      {
-        legacy: true
-      }
-    ],
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-    test && '@babel/plugin-transform-react-jsx-source'
-  ].filter(Boolean)
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
+  ]
 }
