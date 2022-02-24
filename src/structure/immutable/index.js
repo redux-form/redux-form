cde517c55fb1f3910af09b3ecc58a0f2192b613e
Bruno Flamand <bflamand@stingray.com>
2022-02-23 21:08:15 -0500

    fix immutablejs structure

--- src/structure/immutable/index.js
+++ src/structure/immutable/index.js
@@ -1,6 +1,6 @@
 // @flow
 import type { List as ImmutableList, Map as ImmutableMap } from 'immutable'
-import { fromJS, Iterable, List, Map } from 'immutable'
+import { fromJS, isImmutable, isIndexed, List, Map } from 'immutable'
 import { toPath } from 'lodash'
 import type { Structure } from '../../types'
 import plainGetIn from '../plain/getIn'
@@ -14,7 +14,7 @@ const structure: Structure<ImmutableMap<string, any>, ImmutableList<any>> = {
   empty: Map(),
   emptyList: List(),
   getIn: (state: ImmutableMap<string, any> | ImmutableList<any>, field: string) =>
-    Iterable.isIterable(state) ? state.getIn(toPath(field)) : plainGetIn(state, field),
+    isImmutable(state) ? state.getIn(toPath(field)) : plainGetIn(state, field),
   setIn,
   deepEqual,
   deleteIn: (state: ImmutableMap<string, any> | ImmutableList<any>, field: string) =>
@@ -23,14 +23,14 @@ const structure: Structure<ImmutableMap<string, any>, ImmutableList<any>> = {
     items.forEach(callback)
   },
   fromJS: jsValue =>
-    fromJS(jsValue, (key, value) => (Iterable.isIndexed(value) ? value.toList() : value.toMap())),
+    fromJS(jsValue, (key, value) => (isIndexed(value) ? value.toList() : value.toMap())),
   keys,
   size: list => (list ? list.size : 0),
   some: (items, callback) => items.some(callback),
   splice,
   equals: (a, b) => (b.equals(a) ? true : b.toSet().equals(a.toSet())),
   orderChanged: (a, b) => b.some((val, index) => val !== a.get(index)),
-  toJS: value => (Iterable.isIterable(value) ? value.toJS() : value)
+  toJS: value => (isImmutable(value) ? value.toJS() : value)
 }
 
 export default structure
